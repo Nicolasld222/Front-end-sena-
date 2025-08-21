53f84fde4da7d8cf72aa3e2bc1ab57729d323de5
Nicolas Londoño Diaz <nicolaslondia@gmail.com>
2025-08-21 07:20:44 -0500

    Update index.ts

--- type-script/Taller3/index.ts
+++ type-script/Taller3/index.ts
@@ -25,7 +25,7 @@ promesa2.then((res)=>{
 */
 
 //Ejemplo 3
-
+/*
 let estracto = 4
 
 let promesa3 = new Promise((resolve, reject)=>{
@@ -41,10 +41,111 @@ let promesa3 = new Promise((resolve, reject)=>{
         reject(`No tiene Derecho a un subsidioo`)
     }
     
+    
+    
 })
 
 promesa3.then((res)=>{
     console.log(res)
 }).catch((err)=>{
     console.log(err)
+})
+*/
+//Ejemplo 4
+/*
+let promesa1 = new Promise ((resolve, reject )=>{
+    resolve('“Somos ADSI”')
+})
+promesa1.then((res)=>{
+    console.log(res)
+})
+
+
+
+let bandera: boolean = false 
+
+let promesa2 = new Promise((resolve, reject)=>{
+
+    if(bandera == true){
+        resolve('Somos programadores')
+    }else{
+        reject('Promesa 2 no cumplida')
+    }
+})
+promesa2.then((res)=>{
+    console.log(res);
+    
+}).catch((err)=>{
+    console.log(err);
+    
+})
+*/
+
+
+//Ejemplo 5
+/*
+let Promesa5 = new Promise((resolve, reject)=>{
+    function banderaAct(){
+        resolve('Promesa resuelta')
+    }
+    setTimeout(banderaAct, 4000);
+    function banderaDesc(){
+        reject(new Error ('info error'))
+    }
+    setTimeout(banderaDesc,4000)
+})
+
+
+
+Promesa5
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.error(err.stack);
+    });
+*/
+
+//Ejemplo 6
+
+
+let promesa1 = new Promise((resolve, reject) => {
+    function resolver() {
+        resolve("soy la promesa 1");
+    }
+    setTimeout(resolver, 5000);
+})
+
+let promesa2 = new Promise((resolve, reject) => {
+    function resolver() {
+        resolve("soy la promesa 2");
+    }
+    setTimeout(resolver, 3000);
+})
+
+let promesa3 = new Promise((resolve, reject) => {
+    function resolver() {
+        resolve("soy la promesa 3");
+    }
+    setTimeout(resolver, 4000);
+
+    
+})
+
+let promesa4 = new Promise((resolve, reject) => {
+    function resolver() {
+        resolve("soy la promesa 4");
+    }
+    setTimeout(resolver, 5000);
+
+    
+})
+
+
+Promise.race([promesa1, promesa2, promesa3, promesa4])
+.then((resultado) => {
+    console.log(resultado);
+}).catch((Error)=>{
+    console.log(Error);
+    
 })
